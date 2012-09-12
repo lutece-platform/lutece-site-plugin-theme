65a223802f658de098cddf04a465c2c23d619131
hohll <hohll@users.noreply.github.com>
2012-09-12 14:18:28 +0000

    LUTECE-1493 : Bootstrap CSS Framework integration Update Theme plugin init_db_theme.sql database script
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-theme@43784 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/theme/plugin/init_db_theme.sql
+++ src/sql/plugins/theme/plugin/init_db_theme.sql
@@ -1,4 +1,8 @@
-INSERT INTO theme_theme VALUES ('black','Thème Noir','themes/black/images/','themes/black/css','Mairie de Paris','http://fr.lutece.paris.fr','1.0','Creative-Commons', 'themes/black/js');
-INSERT INTO theme_theme VALUES ('blue','Thème Bleu','images/','css','Mairie de Paris','http://fr.lutece.paris.fr','1.0','Creative-Commons', 'js');
-
+INSERT INTO theme_theme (code_theme, theme_description, path_images, path_css, theme_author, theme_author_url, theme_version, theme_licence, path_js) VALUES
+	('amelia', 'Amelia', 'themes/amelia/img/', 'themes/amelia/css', 'bootswatch', 'http://bootswatch.com/', '1.0', 'Apache 2.0', 'js/'),
+	('blue', 'Thème Défaut', 'images/', 'css', 'Mairie de Paris', 'http://fr.lutece.paris.fr', '1.0', 'BSD', 'js/'),
+	('slate', 'Slate', 'themes/slate/img/', 'themes/slate/css', 'bootswatch', 'http://bootswatch.com/', '1.0', 'Apache 2.0', 'js/'),
+	('superhero', 'Superhero', 'themes/superhero/img/', 'themes/superhero/css', 'bootswatch', 'http://bootswatch.com/', '1.0', 'Apache 2.0', 'js/'),
+	('united', 'United', 'themes/united/img/', 'themes/united/css', 'bootswatch', 'http://bootswatch.com/', '1.0', 'Apache 2.0', 'js/');
+	
 INSERT INTO theme_global VALUES ('blue');
