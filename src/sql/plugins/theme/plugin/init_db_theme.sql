489e8d475f47b2acf2959edd883a908824762721
pierrelevy <pierrelevy@users.noreply.github.com>
2014-05-29 16:36:33 +0000

    THEME-5 :Upgrades for Bootstrap v3 + THEME-6 : New themes : flatly, darkly, cyborg, readable, cosmo
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-theme@68467 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/theme/plugin/init_db_theme.sql
+++ src/sql/plugins/theme/plugin/init_db_theme.sql
@@ -1,8 +1,13 @@
 INSERT INTO theme_theme (code_theme, theme_description, path_images, path_css, theme_author, theme_author_url, theme_version, theme_licence, path_js) VALUES
-	('amelia', 'Amelia', 'themes/amelia/img/', 'themes/amelia/css', 'bootswatch', 'http://bootswatch.com/', '1.0', 'Apache 2.0', 'js/'),
+	('amelia', 'Amelia', 'themes/amelia/img/', 'themes/amelia/css', 'Bootswatch', 'http://bootswatch.com/', '1.0', 'Apache 2.0', 'js/'),
 	('blue', 'Thème Défaut', 'images/', 'css', 'Mairie de Paris', 'http://fr.lutece.paris.fr', '1.0', 'BSD', 'js/'),
-	('slate', 'Slate', 'themes/slate/img/', 'themes/slate/css', 'bootswatch', 'http://bootswatch.com/', '1.0', 'Apache 2.0', 'js/'),
-	('superhero', 'Superhero', 'themes/superhero/img/', 'themes/superhero/css', 'bootswatch', 'http://bootswatch.com/', '1.0', 'Apache 2.0', 'js/'),
-	('united', 'United', 'themes/united/img/', 'themes/united/css', 'bootswatch', 'http://bootswatch.com/', '1.0', 'Apache 2.0', 'js/');
+	('slate', 'Slate', 'themes/slate/img/', 'themes/slate/css', 'Bootswatch', 'http://bootswatch.com/', '1.0', 'Apache 2.0', 'js/'),
+	('superhero', 'Superhero', 'themes/superhero/img/', 'themes/superhero/css', 'Bootswatch', 'http://bootswatch.com/', '1.0', 'Apache 2.0', 'js/'),
+	('flatly', 'Flatly', 'themes/flatly/img/', 'themes/flatly/css', 'Bootswatch', 'http://bootswatch.com/', '1.0', 'Apache 2.0', 'js/'),
+	('darkly', 'Darkly', 'themes/darkly/img/', 'themes/darkly/css', 'Bootswatch', 'http://bootswatch.com/', '1.0', 'Apache 2.0', 'js/'),
+	('cyborg', 'Cyborg', 'themes/cyborg/img/', 'themes/cyborg/css', 'Bootswatch', 'http://bootswatch.com/', '1.0', 'Apache 2.0', 'js/'),
+	('readable', 'Readable', 'themes/readable/img/', 'themes/readable/css', 'Bootswatch', 'http://bootswatch.com/', '1.0', 'Apache 2.0', 'js/'),
+	('cosmo', 'Cosmo', 'themes/cosmo/img/', 'themes/cosmo/css', 'Bootswatch', 'http://bootswatch.com/', '1.0', 'Apache 2.0', 'js/'),
+	('united', 'United', 'themes/united/img/', 'themes/united/css', 'Bootswatch', 'http://bootswatch.com/', '1.0', 'Apache 2.0', 'js/');
 	
 INSERT INTO theme_global VALUES ('blue');
